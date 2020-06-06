90ae72b2a0f9903ccb1a82364c96dd1150204f07
Zachary Hislop <hislopzach@gmail.com>
2020-06-06 16:03:23 -0700

    add sunk array, don't print unless cup hasn't been sunk

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_vcnl4010.VCNL4010(i2c)
 
 num_sensors = 1
+cup_sunk = [0]*num_sensors
 thresholds = [3100]
 
 
@@ -22,8 +23,9 @@ def main():
     print("initial value: {}".format(sensor.proximity))
     while True:
         for i in range(num_sensors):
-            if sensor.proximity > thresholds[i]:
-                print("Ball in cup")
+            if not cup_sunk[i] and sensor.proximity > thresholds[i]:
+                cup_sunk[i] = 1
+                print("Ball in cup {}".format(i))
 
 
 if __name__ == '__main__':
