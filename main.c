fc802bb8a31d9d7812c3edbe804da3ed468e8a89
Zachary Hislop <hislopzach@gmail.com>
2020-06-06 16:40:19 -0700

    setup gpio pins, ready for mux usage, but currently commented out

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "keypad.h"
 #include "lcd.h"
 #include "string.h"
 
@@ -6,7 +7,11 @@
 #define TURN_DURATION 8000
 #define NUM_ROWS 4
 #define NUM_COLS 4
+#define NUM_CUPS 4
 #define BOARD_SIZE (NUM_COLS * NUM_ROWS)
+
+// times
+#define THIRTY_SEC 5000
 
 /**
  * CPE 329 Final Project - Connect 4 Cup Pong
@@ -17,24 +22,29 @@ typedef enum state
 {
   INIT,
   RESET,
-  GAME_START,
-  GAME_OVER,
-  NEXT_TURN,     // switch current team,
-  CHECK_BOARD,   // check current board to see if anyone has won
+  READY,
+  START_GAME,
+  END_GAME,
+  GAME_PLAY,     // switch current team,
   UPDATE_BOARD,  // write current state of board to LEDs
 } state;
 
 // start => turn => check_board => game_over
 //                             |=> next turn
 
-void reset_array(char* arr, size_t length);
+void reset_string(char* arr, size_t length);
 void zero_array(int* arr, size_t length);
 void board_set(int row, int col, int value);
 int board_get(int row, int col);
 int place_chip(int col, int color);
+void InitSensor(uint8_t device_addr,
+                DIO_PORT_Interruptable_Type* port,
+                uint16_t sda_pin,
+                uint16_t scl_pin);
 // globals
 int board[BOARD_SIZE];    // keeps track of current board statr
 int col_count[NUM_COLS];  // keeps track of number of tokens in each column
+int cup_array[NUM_CUPS];
 
 void main(void)
 {
@@ -42,14 +52,11 @@ void main(void)
   state next_state;
   state previous_state;
 
-  // color stores the color value for each team
-  int color[2] = {1, 2};
+  char top_line[LCD_LINESIZE], bottom_line[LCD_LINESIZE];
 
-  // char top_line[LCD_LINESIZE], bottom_line[LCD_LINESIZE];
-
-  char last_pressed;
-  int digit;
-  int check_key = 1;
+  char key_pressed;
+  int digit_pressed;
+  int digit, timer_count, is_winner = 0;
 
   // state machine:
   // for every loop, perform the actions for the present state
@@ -61,24 +68,55 @@ void main(void)
     {
       case INIT:
         LCD_init();
-        // sensor_init();
+        keypad_init();
+        // sensor_init(0x13, P1, BIT6, BIT7);
+        // sensor_init(0x13, P1, BIT6, BIT7);
+        // sensor_init(0x13, P1, BIT6, BIT7);
+        // sensor_init(0x13, P1, BIT6, BIT7);
         next_state = RESET;
 
       case RESET:
-        printf("Reset state\n");
         LCD_clear();
-        zero_array(board, BOARD_SIZE);
-        // reset_array(top_line, LCD_LINESIZE);
-        // reset_array(bottom_line, LCD_LINESIZE);
+        zero_array(cup_array, NUM_CUPS);
+        reset_string(top_line, LCD_LINESIZE);
+        reset_string(bottom_line, LCD_LINESIZE);
+        is_winner = 0;
+        digit_pressed = 0;
         // go to locked state
-        next_state = LOCKED;
+        next_state = READY;
         break;
-      case INCORRECT_KEY:
-        printf("Incorrect key state\n");
-        LCD_clear();
-        strcpy(top_line, "INCORRECT KEY");
-        LCD_write_strings(top_line, NULL);
-        delayMs(MESSAGE_DURATION);
+      case READY:
+        // prompt for difficulty level
+        // key_pressed = keypad_getkey_blocking();
+        // while (key != "*" && key != "0" && key != "#")
+        digit_pressed = keypad_getint_blocking();
+        // if invalid key pressed do it again
+        while (digit_pressed > 9)
+        {
+          // lcd invalid key
+          // lcd prompt again
+          digit_pressed = keypad_getint_blocking();
+        }
+        timer_count = get_timer_settings(digit_pressed);
+
+        break;
+      case START_GAME:
+        // enable timer interupt
+        // go to game state
+        next_state = GAME_PLAY;
+        break;
+      case GAME_PLAY:
+        // every .5 s
+        // check board for win. if win => game over
+        // update leds for game?
+        // non blocking check for reset key
+        next_state = GAME_PLAY;
+        break;
+      case END_GAME:
+        // check score
+        // print message
+        // disable interrupts
+        // wait for any key, then go to reset
         next_state = RESET;
         break;
     }
@@ -90,7 +128,7 @@ void main(void)
 }
 
 /* Fills a char array with null characters */
-void reset_array(char* arr, size_t length)
+void reset_string(char* arr, size_t length)
 {
   int i;
   // make buffer all null chars
@@ -133,4 +171,51 @@ int place_chip(int col, int color)
   {
     return -1;
   }
+}
+
+uint8_t ReadEEPROM(uint16_t MemAddress)
+{
+  uint8_t ReceiveByte;
+  uint8_t HiAddress;
+  uint8_t LoAddress;
+
+  HiAddress = MemAddress >> 8;
+  LoAddress = MemAddress & 0xFF;
+
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR;     // Set transmit mode (write)
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;  // I2C start condition
+
+  while (!TransmitFlag)
+    ;  // Wait for EEPROM address to transmit
+  TransmitFlag = 0;
+
+  EUSCI_B0->TXBUF = HiAddress;  // Send the high byte of the memory address
+
+  while (!TransmitFlag)
+    ;  // Wait for the transmit to complete
+  TransmitFlag = 0;
+
+  EUSCI_B0->TXBUF = LoAddress;  // Send the low byte of the memory address
+
+  while (!TransmitFlag)
+    ;  // Wait for the transmit to complete
+  TransmitFlag = 0;
+
+  EUSCI_B0->CTLW0 &= ~EUSCI_B_CTLW0_TR;    // Set receive mode (read)
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;  // I2C start condition (restart)
+
+  // Wait for start to be transmitted
+  while ((EUSCI_B0->CTLW0 & EUSCI_B_CTLW0_TXSTT))
+    ;
+
+  // set stop bit to trigger after first byte
+  EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
+
+  while (!TransmitFlag)
+    ;  // Wait to receive a byte
+  TransmitFlag = 0;
+
+  ReceiveByte = EUSCI_B0->RXBUF;  // Read byte from the buffer
+
+  return ReceiveByte;
 }
